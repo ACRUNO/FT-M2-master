6bb58d6acd823c52675ec030c7a1f5684a3c853d
Joaquin Delgado <jotadelgado96@gmail.com>
2022-10-03 11:55:21 -0300

    commit homework 04

--- 04-Ajax/homework/index.js
+++ 04-Ajax/homework/index.js
@@ -1,40 +1,57 @@
 
-$('#boton').click(function(){
+let getAmigos = () => {
     $.get("http://localhost:5000/amigos/", data => {
-        $('ul').empty();
+        $('#lista').empty();
         data.forEach(element => {
             var lista = document.createElement('li');    
-            lista.innerHTML = element.name;   
+            lista.innerHTML = `${element.id}: ${element.name}`;   
             $('#lista').append(lista);    
         });
     });
-});
+}
 
-$('#search').click(function(){
+
+$('#boton').click(getAmigos);
+
+// ASI LO HABIA HECHO YO Y BUSCABA POR NOMBRE. EL EJERCICIO PEDIA POR ID
+/* $('#search').click(() => {
     $.get("http://localhost:5000/amigos/", data =>{
         $('#amigo').empty();
         data.forEach((element, index) => {
             if (element.name === $("#input").val()){
-                $('#amigo').text(element.name);2
+                $('#amigo').text(element.name);
             }
             //  if (element.id === $("#input").val()){
             //      $('#amigo').text(element.name);
             //  }
         })
     })
+}); */
+
+//RESOLUCION DEL PROFE DEL SEARCH
+
+$('#search').click(() => {
+    let index = $('#input').val();
+    $.get(`http://localhost:5000/amigos/${index}`, data =>{
+        $('#amigo').empty();
+        $('#amigo').text(data.name);
+    })
 });
 
 
-$('#delete').click(function(){
+$('#delete').click(() => {
     var index = $("#inputDelete").val();
+    // $('#success').empty();
     $.ajax({
         url: 'http://localhost:5000/amigos/' + index,
         type: 'DELETE',
         success: () =>{
             $('#success').text("Fue borrado con Exito");
+            getAmigos();
         },
         error: () => {
             $('#success').text("Error 404: ID Amigo not found");
+            
         }
     })
 })
