35cc9f95f5cb1dc6be4a0efb712a330d800cf707
Joaquin Delgado <jotadelgado96@gmail.com>
2022-10-05 10:32:03 -0300

    Homework 07 Styles CSS

--- 07-React-Estilos/homework/src/components/Cards.jsx
+++ 07-React-Estilos/homework/src/components/Cards.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Card from './Card.jsx';
+import s from "./Cards.module.css"
 
 export default function Cards(props) {
   // acá va tu código
   // tip, podés usar un map
   return (
-    <div>
+    <div className={s.mainBox}>
         {
           props.cities.map(card => (
             <Card
--- 07-React-Estilos/homework/src/components/SearchBar.jsx
+++ 07-React-Estilos/homework/src/components/SearchBar.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import s from './SearchBar.module.css';
 
 export default function SearchBar(props) {
   // acá va tu código
   return (
-    <div>
+    <div className={s.mainBox}>
       <input id='inp' type='text' placeholder='Ciudad...'/>
-      <button onClick={() => props.onSearch('Buscando...')}>Agregar</button>
+      <button onClick={() => props.onSearch('Buscando...')} className={s.btn}>Agregar</button>
     </div>
   )
 };
